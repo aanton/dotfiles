aa039cf9986d882d54bab57de7f9a41c1a0d4d70
aanton <aanton@gmail.com>
2019-11-08 18:26:59 +0100

    Change ZSH as a previous requirement

--- README.md
+++ README.md
@@ -6,6 +6,20 @@ Inspired by:
 * https://github.com/victoriadrake/dotfiles
 * https://github.com/alrra/dotfiles
 
+## Requirements
+
+* Install ZSH & set it as the default shell
+  * Logout & login are required !
+  * Verify that ZSH is the default shell
+
+```shell
+sudo apt install zsh
+chsh -s "$(which zsh)"
+gnome-session-quit # logout
+
+echo $SHELL
+```
+
 ## Usage
 
 1. Create symlinks
--- install_tools.sh
+++ install_tools.sh
@@ -2,23 +2,17 @@
 
 . `dirname $0`/core.sh
 
-function install_zsh {
-  install_apt zsh
-
-  if [[ ! "$SHELL" == "$(which zsh)" ]]; then
-    echo "Setting ZSH as the default shell"
-    chsh -s "$(which zsh)"
+function install_ohmyzsh {
+  if [[ -d $HOME/.oh-my-zsh ]]; then
+    echo "App already installed: oh-my-zsh"
+    return
   fi
 
-  if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
-    echo "Installing: Oh-my-zsh ..."
-    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
-  fi
+  echo "Installing: oh-my-zsh..."
+  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
 }
 
 sudo apt update
-
-install_zsh
 
 install_apt curl
 install_apt git
@@ -31,3 +25,5 @@ install_apt vim
 install_manual fd
 install_manual fzf
 install_manual ripgrep rg
+
+install_ohmyzsh
