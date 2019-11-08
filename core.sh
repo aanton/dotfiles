f3a7abde99cc7e5b1d0d42674041e65b6e653797
aanton <aanton@gmail.com>
2019-11-08 18:18:27 +0100

    Refactor core.sh

--- core.sh
+++ core.sh
@@ -3,32 +3,34 @@
 PROJECTDIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 DATETIME=$(date +%Y%m%d%H%M%S)
 
-function check_installed {
+function check_app_installed {
   local -r APP=$1
   local -r CHECK=${2:-$1}
 
   which $CHECK &> /dev/null
   if [ $? -ne 0 ]; then
-    echo "Installing: ${APP} ..."
+    echo "App NOT found: ${APP}"
     return 1
   fi
 
-  echo "Already installed: ${APP}"
+  echo "App already installed: ${APP}"
   return 0
 }
 
 function install_apt {
-  check_installed "$@"
+  check_app_installed "$@"
 
   if [ $? -ne 0 ]; then
+    echo "Installing APT app: $1 ..."
     sudo apt install -y $1
   fi
 }
 
 function install_manual {
-  check_installed "$@"
+  check_app_installed "$@"
 
   if [ $? -ne 0 ]; then
+    echo "Installing manual app: $1 ..."
     . ${PROJECTDIR}/tools/$1.sh
   fi
 }
