e2b43d70060288a3d489888713ebe9db58fead24
aanton <aanton@gmail.com>
2019-11-08 14:44:21 +0100

    Avoid creating local configuration files if already exist

--- create_local_configs.sh
+++ create_local_configs.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+function check_file_exists {
+  if [ ! -f "$1" ]; then
+    echo "Creating local file: $1 ..."
+    return 1
+  fi
+
+  echo "File already exists: $1"
+  return 0
+}
+
 function create_gitconfig {
   local -r DESTINATION="${HOME}/.gitconfig.local"
+
+  check_file_exists $DESTINATION
+  if [ $? -eq 0 ]; then
+    return 0
+  fi
 
   read -p "Enter your Git user.name [${USER}]: " NAME
   NAME=${NAME:-$USER}
@@ -19,6 +34,12 @@ TXT
 
 function create_zshrc {
   local -r DESTINATION="${HOME}/.zshrc.local"
+
+  check_file_exists $DESTINATION
+  if [ $? -eq 0 ]; then
+    return 0
+  fi
+
   NAME=${USER}
 
   cat > $DESTINATION <<- TXT
