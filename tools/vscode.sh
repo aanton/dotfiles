e78500d895bcd74eee84cfece261fa251a4284aa
aanton <aanton@gmail.com>
2019-11-08 19:16:10 +0100

    Add configuration files for vscode

--- tools/vscode.sh
+++ tools/vscode.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function create_vscode_symlinks {
+  create_symlink vscode/settings.json .config/Code/User/settings.json
+  create_symlink vscode/keybindings.json .config/Code/User/keybindings.json
+  create_symlink vscode/snippets .config/Code/User/snippets
+}
+
 curl -L https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -o /tmp/vscode.deb
 sudo dpkg -i /tmp/vscode.deb
 rm /tmp/vscode.deb
+
+create_vscode_symlinks
