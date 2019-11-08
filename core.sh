9b06f2b2dd58c3c3a18d296842848894adb081ec
aanton <aanton@gmail.com>
2019-11-08 15:31:22 +0100

    Move functions to core.sh

--- core.sh
+++ core.sh
@@ -2,3 +2,33 @@
 
 PROJECTDIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 DATETIME=$(date +%Y%m%d%H%M%S)
+
+function check_installed {
+  local -r APP=$1
+  local -r CHECK=${2:-$1}
+
+  which $CHECK &> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${APP} ..."
+    return 1
+  fi
+
+  echo "Already installed: ${APP}"
+  return 0
+}
+
+function install_apt {
+  check_installed "$@"
+
+  if [ $? -ne 0 ]; then
+    sudo apt install -y $1
+  fi
+}
+
+function install_manual {
+  check_installed "$@"
+
+  if [ $? -ne 0 ]; then
+    . ${PROJECTDIR}/tools/$1.sh
+  fi
+}
--- install_tools.sh
+++ install_tools.sh
@@ -2,36 +2,6 @@
 
 . `dirname $0`/core.sh
 
-function check_installed {
-  local -r APP=$1
-  local -r CHECK=${2:-$1}
-
-  which $CHECK &> /dev/null
-  if [ $? -ne 0 ]; then
-    echo "Installing: ${APP} ..."
-    return 1
-  fi
-
-  echo "Already installed: ${APP}"
-  return 0
-}
-
-function install_apt {
-  check_installed "$@"
-
-  if [ $? -ne 0 ]; then
-    sudo apt install -y $1
-  fi
-}
-
-function install_manual {
-  check_installed "$@"
-
-  if [ $? -ne 0 ]; then
-    . ${PROJECTDIR}/tools/$1.sh
-  fi
-}
-
 function install_zsh {
   install_apt zsh
 
