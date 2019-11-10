2413758fcce5f2daaf569976c3cec92e6f1ba498
aanton <aanton@gmail.com>
2019-11-10 22:45:48 +0100

    Extract vscode extensions list to a text file

--- tools/vscode.sh
+++ tools/vscode.sh
@@ -12,24 +12,12 @@ function create_vscode_symlinks {
   create_symlink vscode/snippets .config/Code/User/snippets
 }
 
-function install_vscode_extension {
-  code --install-extension $1 --force
+function install_vscode_extensions {
+  for EXTENSION in $(cat ${PROJECTDIR}/tools/vscode-extensions.txt); do
+    code --install-extension $EXTENSION --force
+  done
 }
 
 install_vscode
-
 create_vscode_symlinks
-
-install_vscode_extension bierner.emojisense
-install_vscode_extension bmewburn.vscode-intelephense-client
-install_vscode_extension dakara.transformer
-install_vscode_extension eamodio.gitlens
-install_vscode_extension HookyQR.beautify
-install_vscode_extension ms-azuretools.vscode-docker
-install_vscode_extension oderwat.indent-rainbow
-install_vscode_extension richie5um2.vscode-sort-json
-install_vscode_extension ryu1kn.partial-diff
-install_vscode_extension sleistner.vscode-fileutils
-install_vscode_extension softwareape.numbermonger
-install_vscode_extension Tyriar.lorem-ipsum
-install_vscode_extension zhuangtongfa.Material-theme
+install_vscode_extensions
