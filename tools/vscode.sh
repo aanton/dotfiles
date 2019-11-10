686c47583133b03b20108686b0c1341ea4d0563e
aanton <aanton@gmail.com>
2019-11-10 19:40:43 +0100

    Extract function install_vscode

--- tools/vscode.sh
+++ tools/vscode.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function install_vscode {
+  curl -L https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -o /tmp/vscode.deb
+  sudo dpkg -i /tmp/vscode.deb
+  rm /tmp/vscode.deb
+}
+
 function create_vscode_symlinks {
   create_symlink vscode/settings.json .config/Code/User/settings.json
   create_symlink vscode/keybindings.json .config/Code/User/keybindings.json
@@ -10,9 +16,7 @@ function install_vscode_extension {
   code --install-extension $1 --force
 }
 
-curl -L https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -o /tmp/vscode.deb
-sudo dpkg -i /tmp/vscode.deb
-rm /tmp/vscode.deb
+install_vscode
 
 create_vscode_symlinks
 
