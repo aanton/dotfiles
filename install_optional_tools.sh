8c2297b3ee12c5dc04f55cbd93850c22826774db
aanton <aanton@gmail.com>
2019-11-10 13:04:26 +0100

    Install FileZilla

--- install_optional_tools.sh
+++ install_optional_tools.sh
@@ -4,4 +4,8 @@
 
 sudo apt update
 
+echo "Installing optional development tools ..."
 ask_for_installation docker && install_manual docker
+
+echo "Installing optional desktop tools ..."
+ask_for_installation filezilla && install_apt filezilla
