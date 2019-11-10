4efe571bcef13d6299fd94ae67896c4f641b52ab
aanton <aanton@gmail.com>
2019-11-10 20:53:31 +0100

    Install Node.js

--- install_optional_tools.sh
+++ install_optional_tools.sh
@@ -8,6 +8,7 @@ echo "Installing optional development tools ..."
 ask_for_installation docker && install_manual docker
 ask_for_installation "php & php extensions" && install_manual php
 ask_for_installation "composer" && install_manual composer
+ask_for_installation node && install_manual node
 
 echo "Installing optional desktop tools ..."
 ask_for_installation filezilla && install_apt filezilla
