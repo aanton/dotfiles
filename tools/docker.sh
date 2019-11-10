739aaa3b9c07fd3c527165c0470d6422e79ff1a1
aanton <aanton@gmail.com>
2019-11-10 12:54:22 +0100

    Fix removing docker script after installation

--- tools/docker.sh
+++ tools/docker.sh
@@ -2,7 +2,7 @@
 
 curl -fsSL https://get.docker.com -o /tmp/docker.sh
 sh /tmp/docker.sh
-/tmp/docker.sh
+rm /tmp/docker.sh
 
 # Executing docker without sudo (logout is required)
 sudo usermod -aG docker ${USER}
