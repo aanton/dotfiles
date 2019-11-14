362ab0e8ebcdb047449c189eac27add4b8cf093f
aanton <aanton@gmail.com>
2019-11-14 17:49:04 +0100

    Install byzanz optionally

--- commands/install_desktop_tools.sh
+++ commands/install_desktop_tools.sh
@@ -8,12 +8,12 @@ function install_desktop_tools {
     return 0
   fi
 
-  install_apt byzanz byzanz-record
   install_apt meld
   install_manual clipit
   install_manual google-chrome
   install_manual vscode code
 
   print_info "Installing optional desktop tools ..."
+  ask_for_installation byzanz && install_apt byzanz byzanz-record
   ask_for_installation filezilla && install_apt filezilla
 }
