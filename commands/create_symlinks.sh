6b414e52c5742df328e101112d75837a933e951c
aanton <aanton@gmail.com>
2019-11-15 09:59:37 +0100

    Create destination directory of symlinks

--- commands/create_symlinks.sh
+++ commands/create_symlinks.sh
@@ -10,12 +10,14 @@ function create_common_symlinks {
 
 function create_clipit_symlinks {
   if [ $(which clipit) ]; then
+    mkdir -p ${HOME}/.config/clipit
     create_symlink clipit/clipitrc .config/clipit/clipitrc
   fi
 }
 
 function create_vscode_symlinks {
   if [ $(which code) ]; then
+    mkdir -p ${HOME}/.config/Code/User
     create_symlink vscode/settings.json .config/Code/User/settings.json
     create_symlink vscode/keybindings.json .config/Code/User/keybindings.json
     create_symlink vscode/snippets .config/Code/User/snippets
