df02748ebffa597da2d1e3340eea4bf2850e7924
aanton <aanton@gmail.com>
2020-01-23 22:48:59 +0100

    New function to install NPM packages

--- commands/install_dev_tools.sh
+++ commands/install_dev_tools.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+#### AUXILIARY FUNCTIONS #######################################################
+
+function install_npm_package {
+  local -r PACKAGE=$1
+  local -r ASK=${2:-"true"}
+
+  if [[ ! $(which npm) ]]; then
+    print_warning "Package ${PACKAGE} can NOT be installed because npm is missing"
+    return 1
+  fi
+
+  if [[ $(npm list -g --depth=0 | fgrep " ${PACKAGE}@") ]]; then
+    print_info "Package already installed: ${PACKAGE}"
+    return 0
+  fi
+
+  if [[ "${ASK}" == "true" ]]; then
+    ask_for_installation ${PACKAGE} || return 1
+  fi
+
+  print_info "Installing package: ${PACKAGE} ..."
+  sudo npm install -g ${PACKAGE}
+}
+
 #### COMMAND ###################################################################
 
 function install_dev_tools {
