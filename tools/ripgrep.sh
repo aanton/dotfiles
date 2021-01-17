803660522a3388a06b4ef29207fac52622bca766
aanton <aanton@gmail.com>
2021-01-17 17:20:03 +0100

    Update ripgrep version

--- tools/ripgrep.sh
+++ tools/ripgrep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb -o /tmp/ripgrep.deb
+curl -L https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb -o /tmp/ripgrep.deb
 sudo dpkg -i /tmp/ripgrep.deb
 rm /tmp/ripgrep.deb
