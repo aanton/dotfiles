e8de98802e27f1c7f63e4bf968ed5cde5391f177
aanton <aanton@gmail.com>
2021-01-17 17:19:17 +0100

    Update bat version

--- tools/bat.sh
+++ tools/bat.sh
@@ -4,7 +4,7 @@ dpkg -s bat &> /dev/null
 if [ $? -eq 0 ]; then
   sudo apt-get install -y bat
 else
-  curl -L https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -o /tmp/bat.deb
+  curl -L https://github.com/sharkdp/bat/releases/download/v0.17.1/bat_0.17.1_amd64.deb -o /tmp/bat.deb
   sudo dpkg -i /tmp/bat.deb
   rm /tmp/bat.deb
 fi
