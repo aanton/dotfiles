fdd1fa2e2e09d8612ae50c4be21b56712bdbd11a
aanton <aanton@gmail.com>
2021-04-11 16:33:39 +0200

    Use latest version of Docker Compose

--- tools/docker-compose.sh
+++ tools/docker-compose.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -Po "\d+\.\d+\.\d+$" | sort --version-sort | tail -n1`
+sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
