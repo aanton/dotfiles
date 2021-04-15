8f58e7d4a3864096f0d9f15c08698c7ea129cd0f
aanton <aanton@gmail.com>
2021-04-15 17:00:04 +0200

    Update PHP version: 8.0

--- tools/php.sh
+++ tools/php.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt install -y php-cli php-common
+sudo add-apt-repository ppa:ondrej/php -y
 
-print_info "Installing php extensions ..."
-sudo apt install -y php-curl php-json php-mbstring php-mysql php-opcache php-readline php-sqlite3 php-xml
+print_info "Installing php8.0 ..."
+sudo apt install -y php8.0-cli php8.0-common
+
+print_info "Installing php8.0 extensions ..."
+sudo apt install -y php8.0-curl php8.0-mbstring php8.0-mysql php8.0-opcache php8.0-readline php8.0-sqlite3 php8.0-xml
