2b6e8f27a8dda32bb14ccb31668976d6fdf67efb
aanton <aanton@gmail.com>
2021-05-26 13:36:48 +0200

    Fix removing Google Chrome package after installation

--- tools/google-chrome.sh
+++ tools/google-chrome.sh
@@ -2,4 +2,4 @@
 
 curl -fsSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome.deb
 sudo dpkg -i /tmp/google-chrome.deb
-rm /tmp/google-chrome.sh
+rm /tmp/google-chrome.deb
