37dd590fe8362fb008c80c9cf0f3b1330de0ccef
aanton <aanton@gmail.com>
2022-10-01 17:36:55 +0200

    Update tools versions

--- tools/bat.sh
+++ tools/bat.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-dpkg -s bat &> /dev/null
-if [ $? -eq 0 ]; then
-  sudo apt-get install -y bat
-else
-  curl -L https://github.com/sharkdp/bat/releases/download/v0.17.1/bat_0.17.1_amd64.deb -o /tmp/bat.deb
-  sudo dpkg -i /tmp/bat.deb
-  rm /tmp/bat.deb
-fi
+curl -L https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb -o /tmp/bat.deb
+sudo dpkg -i /tmp/bat.deb
+rm /tmp/bat.deb
