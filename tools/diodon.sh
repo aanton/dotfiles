9d4077758350862fedb97175ad7e93e00d17cdd2
aanton <aanton@gmail.com>
2022-10-02 11:41:33 +0200

    Install diodon (clipboard manager)

---
+++ tools/diodon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:diodon-team/stable -y
+sudo apt-get install -y diodon
+
+# https://esite.ch/2015/07/using-custom-shortcuts-of-de-as-diodon-hotkey/
+print_info "Create a custom keyboard shortcut to open Diodon clipboard"
