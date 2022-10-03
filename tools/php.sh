602dda6bdf7eb55e1f7907ed90a486d5273bc0dd
aanton <aanton@gmail.com>
2022-10-03 19:28:04 +0200

    Install SOAP PHP extension

--- tools/php.sh
+++ tools/php.sh
@@ -6,4 +6,4 @@ print_info "Installing php8.0 ..."
 sudo apt install -y php8.0-cli php8.0-common
 
 print_info "Installing php8.0 extensions ..."
-sudo apt install -y php8.0-curl php8.0-mbstring php8.0-mysql php8.0-opcache php8.0-readline php8.0-sqlite3 php8.0-xml
+sudo apt install -y php8.0-curl php8.0-mbstring php8.0-mysql php8.0-opcache php8.0-readline php8.0-sqlite3 php8.0-xml php8.0-soap
