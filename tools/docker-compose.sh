3721efb66db847c21bd822cf293cc6942b8d72ea
aanton <aanton@gmail.com>
2024-03-07 09:40:23 +0100

    Update Docker Compose

--- tools/docker-compose.sh
+++ tools/docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -Po "\d+\.\d+\.\d+$" | sort --version-sort | tail -n1`
-sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
