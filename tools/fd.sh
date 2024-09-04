7662501fc071a824adc931bef670be0eb0f8036b
aanton <aanton@gmail.com>
2024-09-04 16:27:56 +0200

    Update fd version

--- tools/fd.sh
+++ tools/fd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L https://github.com/sharkdp/fd/releases/download/v8.4.0/fd_8.4.0_amd64.deb -o /tmp/fd.deb
+curl -L https://github.com/sharkdp/fd/releases/download/v10.2.0/fd_10.2.0_amd64.deb -o /tmp/fd.deb
 sudo dpkg -i /tmp/fd.deb
 rm /tmp/fd.deb
