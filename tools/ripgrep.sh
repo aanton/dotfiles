c1cec4f10e4029158fa42a0c5caf8b161c6c452b
aanton <aanton@gmail.com>
2024-09-04 16:25:12 +0200

    Remove custom installation of ripgrep

--- tools/ripgrep.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -L https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb -o /tmp/ripgrep.deb
-sudo dpkg -i /tmp/ripgrep.deb
-rm /tmp/ripgrep.deb
