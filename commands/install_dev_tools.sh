a89e19d04b7afa87378170a210740b6711dba544
aanton <aanton@gmail.com>
2024-09-04 16:33:57 +0200

    Remove tool docker-compose (already included in docker)

--- commands/install_dev_tools.sh
+++ commands/install_dev_tools.sh
@@ -6,7 +6,6 @@ function install_dev_tools {
   install_app shellcheck
 
   install_app_with_confirmation docker
-  install_app_with_confirmation docker-compose
   install_app_with_confirmation php
   install_app_with_confirmation composer
   install_app_with_confirmation nvm
